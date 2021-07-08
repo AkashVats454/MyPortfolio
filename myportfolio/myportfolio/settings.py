f6e781a7a1d4e760c3c62a900608d1953c55e6bd
AKASH MISHRA <akash.mishra2@sky.uk>
2021-07-08 22:27:14 +0530

    debug change

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'flnt8d!g-sj(2&%l=4!0e=q!exdjwf$50azdarx9te0o16c1wx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'akashmishra454@gmail.com'
